972edf403ad74f7af2f211b54c60f0fcc28316eb
AlanMalikov777 <77801087+AlanMalikov777@users.noreply.github.com>
2021-09-19 02:02:55 +0600

    Task1
    Task1

---
+++ first.py
@@ -0,0 +1,24 @@
+
+def toSwap(listToEnter,x,y):#swaps two variables
+    listToEnter[x],listToEnter[y]=listToEnter[y],listToEnter[x]
+# toPermute uses a heap's algorithm
+def toPermute(list, current, last):#takes a list, starting index and last index
+    if last<0:
+        print("")
+    elif current == last:# if starting and last indexes will equalise then our function will be done
+	    print(''.join(list))
+    else:
+	    for i in range(current, last + 1):#permutating with recursion
+		    toSwap(list,current,i)
+		    toPermute(list, current + 1, last)
+		    toSwap(list,current,i)
+
+txt=input("Inseart your text: ")
+size = len(txt)
+list1 = list(txt)
+toPermute(list1, 0, size-1)
+
+
+
+
+
